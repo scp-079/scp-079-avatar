c345cff813e752fc4302941f5b441a52f8cf213f
Xiao <xiao@scp-079.org>
2019-09-23 09:33:45 +0000

    - modify send_document's parameter name

--- plugins/functions/telegram.py
+++ plugins/functions/telegram.py
@@ -214,7 +214,7 @@ def resolve_peer(client: Client, pid: Union[int, str]) -> Optional[Union[bool, I
     return result
 
 
-def send_document(client: Client, cid: int, file: str, file_ref: str = None, text: str = None, mid: int = None,
+def send_document(client: Client, cid: int, document: str, file_ref: str = None, text: str = None, mid: int = None,
                   markup: InlineKeyboardMarkup = None) -> Optional[Union[bool, Message]]:
     # Send a document to a chat
     result = None
@@ -225,7 +225,7 @@ def send_document(client: Client, cid: int, file: str, file_ref: str = None, tex
             try:
                 result = client.send_document(
                     chat_id=cid,
-                    document=file,
+                    document=document,
                     file_ref=file_ref,
                     caption=text,
                     parse_mode="html",
