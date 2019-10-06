649b9f80ce6444ae1035ed7b99cf966b0a80a3d6
Xiao <xiao@scp-079.org>
2019-10-06 05:47:21 +0000

    - modify share_data

--- plugins/functions/channel.py
+++ plugins/functions/channel.py
@@ -69,8 +69,8 @@ def send_help(client: Client, cid: int, text: str) -> bool:
     return False
 
 
-def share_data(client: Client, receivers: List[str], action: str, action_type: str, data: Union[bool, dict, int, str],
-               file: str = None, encrypt: bool = True) -> bool:
+def share_data(client: Client, receivers: List[str], action: str, action_type: str,
+               data: Union[bool, dict, int, str] = None, file: str = None, encrypt: bool = True) -> bool:
     # Use this function to share data in the channel
     try:
         if glovar.sender in receivers:
