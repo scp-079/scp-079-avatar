09319e9a5c52542267d9c538cee34ef2646f53be
Xiao <xiao@scp-079.org>
2019-10-17 12:13:00 +0000

    - fix bugs

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -217,9 +217,14 @@ def is_emoji(the_type: str, text: str) -> bool:
     # Check the emoji type
     try:
         emoji_dict = {}
-        emoji_list = [emoji for emoji in glovar.emoji_set if emoji in text and emoji not in glovar.emoji_protect]
+        emoji_set = {emoji for emoji in glovar.emoji_set if emoji in text and emoji not in glovar.emoji_protect}
+        emoji_old_set = deepcopy(emoji_set)
 
-        for emoji in emoji_list:
+        for emoji in emoji_old_set:
+            if any(emoji in emoji_old and emoji != emoji_old for emoji_old in emoji_old_set):
+                emoji_set.discard(emoji)
+
+        for emoji in emoji_set:
             emoji_dict[emoji] = text.count(emoji)
 
         # Check ad
