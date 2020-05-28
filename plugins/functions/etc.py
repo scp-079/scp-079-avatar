ce0bc876aa2bc2fe5ff1ebfe614067025cd266fe
Xiao <xiao@scp-079.org>
2020-05-28 12:40:49 +0000

    略做修改

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -229,14 +229,14 @@ def mention_id(uid: int) -> str:
 
 def random_str(i: int) -> str:
     # Get a random string
-    text = ""
+    result = ""
 
     try:
-        text = "".join(choice(ascii_letters + digits) for _ in range(i))
+        result = "".join(choice(ascii_letters + digits) for _ in range(i))
     except Exception as e:
         logger.warning(f"Random str error: {e}", exc_info=True)
 
-    return text
+    return result
 
 
 def t2t(text: str, normal: bool, printable: bool, pure: bool = False) -> str:
