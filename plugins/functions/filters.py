283039ddc0ee3810928b495334178d9f3aea72ac
Xiao <xiao@scp-079.org>
2020-06-03 07:55:44 +0000

    略做修改

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -62,6 +62,9 @@ def is_authorized_group(_, update: Union[CallbackQuery, Message]) -> bool:
 
         cid = message.chat.id
 
+        if cid > 0:
+            return False
+
         if init_group_id(cid):
             return True
     except Exception as e:
